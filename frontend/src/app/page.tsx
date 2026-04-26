0c232a3da127c3751cb8fcae1ec337d816fe106c
kunalkumar-13 <kunalsain0324@gmail.com>
2026-04-26 13:52:04 +0530

    Add React/Next.js UI with animated shader background

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import AnoAI from "@/components/ui/animated-shader-background";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-black">
+      <AnoAI />
+    </div>
+  );
+}
